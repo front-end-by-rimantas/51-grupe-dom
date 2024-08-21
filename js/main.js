27b01404c9ce19568a0b89d888affb016c7acab7
Rimantas <belauzas@gmail.com>
2024-08-21 09:41:59 +0300

    Up

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// 1. susirandi DOMinanti elementa
+// 2. perskaitai jame esancia informacija
+
+// 1. susirandi DOMinanti elementa
+// 2. susikonstruoji nauja HTML (string)
+// 3. HTML istatai i norima vieta ir taip
+// gauni jauna/jaunus elementus
+
+// 1. susirandi DOMinanti elementa
+// 2. ji, arba jame esanti turini - pasalini/modifikuoji
+
+// CRUD:
+// create
+// read
+// update
+// delete
+
+const homeDOM = document.getElementById('home');
+console.log(homeDOM);
